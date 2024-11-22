f358b8e0f57505b671c7e0d0baaea2fe99d054d2
yahel <yahelcohenko@gmail.com>
2024-11-22 19:18:49 +0200

    add wizdomsubs api code from somewhere

---
+++ wizdomsubs/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='wizdomsubs',
+    version='0.1.0',
+    requires=[""]
+    author='wizupdate@github.com, yahel-ck@github.com',
+    description='API for wizdom.xyz, download Hebrew subtitles.',
+)
